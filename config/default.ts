43e1bfd7786f0d2726f3080d69e10ab11fe6c890
simon-tanna <gcab022109@coderacademy.edu.au>
2022-08-11 14:22:37 +1000

    created initial folders and config file for api

---
+++ config/default.ts
@@ -0,0 +1,4 @@
+export default {
+    port: 1337,
+    dbUri: "mongodb://localhost/typescript_tutorial_express"
+}
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import config from 'config';
+
+const app = express();
+const port = config.get<number>('port')
+
+app.listen(port, ()=> {
+    console.log("app listening on port 1337")
+})
